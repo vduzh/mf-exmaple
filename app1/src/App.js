afb1d379affcc8c4eb3f3fe7ffe63f60e7c713e7
vduzh <6265287+vduzh@users.noreply.github.com>
2023-02-20 13:29:09 +0300

    1 file modified

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,26 +1,28 @@
-import React, { Suspense } from "react";
-const RemoteApp = React.lazy(() => import("app2/App"));
-const RemoteApp3 = React.lazy(() => import("app3/App"));
+import React, { Suspense } from 'react';
+const RemoteApp = React.lazy(() => import('app2/App'));
+const RemoteApp3 = React.lazy(() => import('app3/App'));
 
 const App = () => {
   return (
     <div>
-      <div style={{
-        margin: "10px",
-        padding: "10px",
-        textAlign: "center",
-        backgroundColor: "greenyellow"
-      }}>
+      <div
+        style={{
+          margin: '10px',
+          padding: '10px',
+          textAlign: 'center',
+          backgroundColor: 'greenyellow',
+        }}
+      >
         <h1>App1</h1>
       </div>
-      <Suspense fallback={"loading..."}>
+      <Suspense fallback={'loading...'}>
         <RemoteApp />
       </Suspense>
-      <Suspense fallback={"loading..."}>
+      {/* <Suspense fallback={"loading..."}>
         <RemoteApp3 />
-      </Suspense>
-    </div>)
-}
-
+      </Suspense> */}
+    </div>
+  );
+};
 
 export default App;

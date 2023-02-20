90bc68416a3e9830af971496d74e70234346bd72
vduzh <vduzh@tut.by>
2023-02-20 13:08:29 +0300

    Working...

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,19 +1,23 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 const RemoteApp = React.lazy(() => import("app2/App"));
+const RemoteApp3 = React.lazy(() => import("app3/App"));
 
 const App = () => {
   return (
     <div>
       <div style={{
-        margin:"10px",
-        padding:"10px",
-        textAlign:"center",
-        backgroundColor:"greenyellow"
+        margin: "10px",
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: "greenyellow"
       }}>
         <h1>App1</h1>
       </div>
       <Suspense fallback={"loading..."}>
-        <RemoteApp/>
+        <RemoteApp />
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <RemoteApp3 />
       </Suspense>
     </div>)
 }
